7ac6f31c9b80aca830b56a50c45cd093b82fab59
John Castaneda <jaclass08@hotmail.com>
2013-09-05 19:52:28 -0500

    Header

---
+++ header.php
@@ -0,0 +1,42 @@
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+ <title><?php echo $pageTitle; ?></title>
+
+   <link rel="stylesheet" href="grid.css" type="text/css" media="screen">
+<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+</head>
+
+<body>
+
+
+
+
+<div class="container clearfix">
+    
+</br>
+	<div class="grid_9"> 
+		 <span><h1 id="fontchange">  </span> <span id="fontchange2"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<?php echo $pageTitle2; ?></span></h1>
+		 <p id="borderborder"> </p>
+	</div>
+
+
+</br>
+
+</br>
+
+
+	<div class="grid_3 alpha" id="divchange">	
+		<a href="index.php" ><img id="borderradius"class="grid_13" src="HOME.jpg"></a>
+		<a href="blog.php"><img  id="borderradius"class="grid_13"src="BLOG.jpg"></a> 
+		<a href="aboutme.php"><img  id="borderradius"class="grid_13"src="AM.jpg"></a>
+		<a href="contact.php"><img  id="borderradius"class="grid_13"src="SD.jpg"></a>
+
+
+	</div>
+
+
