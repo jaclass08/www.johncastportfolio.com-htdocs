d8068fa3b4a32b63776ec98b765180a143a8f19d
John Castaneda <jaclass08@hotmail.com>
2013-09-05 19:45:09 -0500

    Blog Posting (Will type blog manually)

---
+++ blog.php
@@ -0,0 +1,14 @@
+<?php $pageTitle = "Blog"; ?>
+<?php $pageTitle2 = "BLOG"; ?>
+<?php include('header.php'); ?>
+
+<div >
+		<h2 id="orangefont" class="grid_4"> NO POSTS YET</h2>
+
+
+</br>
+
+</div>
+
+
+<?php include('footer.php'); ?>
