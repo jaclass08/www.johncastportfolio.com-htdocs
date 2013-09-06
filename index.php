d0c14d6a1644a1ea8cf99cd2d597a0d7e1467ff0
John Castaneda <jaclass08@hotmail.com>
2013-09-05 19:53:09 -0500

    Front Page

---
+++ index.php
@@ -0,0 +1,78 @@
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>John Castaneda</title>
+
+   <link rel="stylesheet" href="grid.css" type="text/css" media="screen">
+<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+   <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+</head>
+
+<body>
+
+
+
+
+<div class="container clearfix">
+    
+</br>
+	<div class="grid_9"> 
+		 <span><h1 id="fontchange"> &nbsp John Castaneda</span> <span id="fontchange2">Computer Science Graduate</span></h1>
+		 <p id="borderborder"> </p>
+	</div>
+
+
+</br>
+
+</br>
+
+
+	<div class="grid_3 alpha" id="divchange">	
+		<a href="index.php" ><img id="borderradius"class="grid_13" src="HOME.jpg"></a>
+		<a href="blog.php"><img  id="borderradius"class="grid_13"src="BLOG.jpg"></a> 
+		<a href="aboutme.php"><img  id="borderradius"class="grid_13"src="AM.jpg"></a>
+		<a href="contact.php"><img  id="borderradius"class="grid_13"src="SD.jpg"></a>
+
+
+	</div>
+
+
+<div >
+		<h2 id="orangefont" class="grid_4"> Currently Seeking: </h2>
+		<p class="grid_16"> 1. A career in web development, programming, QA testing, or database administration.
+		</p>
+		<p class="grid_16"> 2. Potential clients in need of a website.</p>
+
+</br>
+
+</div>
+
+
+</br>
+
+</br></br>
+
+</br></br>
+
+</br></br>
+
+</br></br>
+
+
+
+
+<div>
+<h2 id="orangefont" class="grid_4"> Why hire me? </h2>
+		<p class="grid_9"> 
+
+			Would you hire anybody who thought about their projects non-stop throughout the day, who challenges their mind endlessly, thinks creatively, and who strives to become successful in life? I think I might just fit that category.
+
+		</p>
+</div>
+
+
+
+<?php include('footer.php'); ?>
